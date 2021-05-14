a1040e77f65b8a75e3073e0d8f5a5ccdff6ecb6b
htnafhD <81007831+htnafhD@users.noreply.github.com>
2021-05-14 16:10:20 +0700

    Update caesar.py
    update final

--- caesar.py
+++ caesar.py
@@ -2,25 +2,22 @@ LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 
 def encrypt():
-    text = input("Enter plain: ")
+    text = input("Enter plain: ").strip()
     s = int(input("Shift: "))
     result = ""
     for i in range(len(text)):
         char = text[i]
         if (char.isupper()):
-            result += chr((ord(char) + s-65) % 26 + 65)
+            result += chr((ord(char) + s - 65) % 26 + 65)
         else:
             result += chr((ord(char) + s - 97) % 26 + 97)
     print("Cipher: ", result)
 
 
-def decrypt(LETTERS):
-    result = ""
-    encrypted_message = input(
-        "Enter the message you would like to decrypt: ").strip()
-    key = int(input("Enter key to decrypt: "))
-
+def decrypt(LETTERS, key):
     decrypted_message = ""
+    encrypted_message = input(
+        "Enter encrypted message:").strip()
 
     for c in encrypted_message:
         if c in LETTERS:
@@ -30,30 +27,44 @@ def decrypt(LETTERS):
             decrypted_message += new_character
         else:
             decrypted_message += c
-    print(decrypted_message)
+    print("DECODE: ", decrypted_message)
 
 
-def brute_force(LETTERS):
-    text = input("Enter plaint text:")
-    for key in range(len(LETTERS)):
-        translated = ''
-        for symbol in text:
-            if symbol in LETTERS:
-                num = LETTERS.find(symbol)
-                num = num - key
-                if num < 0:
-                    num = num + len(LETTERS)
-                translated = translated + LETTERS[num]
-            else:
-                translated = translated + symbol
-        print('DECODE #%s: %s' % (key, translated))
+def brute_force(LETTERS, encrypted_message, key):
+    decrypted_message = ""
+    for c in encrypted_message:
+        if c in LETTERS:
+            position = LETTERS.find(c)
+            new_position = (position - key) % 26
+            new_character = LETTERS[new_position]
+            decrypted_message += new_character
+        else:
+            decrypted_message += c
+    print('DECODE %s: %s' % (key, decrypted_message))
+
+
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, x, y = egcd(b % a, a)
+        return g, y - (b // a) * x, x
+
+
+def inverse_mod(a, n):
+    for x in range(1, n):
+        if ((a % n) * (x % n)) % n == 1:
+            return x
+    return -1
 
 
 def menu():
     print("1. Encrypt")
-    print("\n2. Decrypt")
-    print("\n3. BruteForce\n")
-    x = int(input("Enter choice:"))
+    print("2. Decrypt")
+    print("3. BruteForce")
+    print("4. Euclidean Algorithm")
+    print("5. Modular Inverse")
+    x = int(input("\nEnter choice: "))
     return x
 
 
@@ -63,9 +74,20 @@ def main():
         if z == 1:
             encrypt()
         elif z == 2:
-            decrypt(LETTERS)
+            key = int(input("Shift: "))
+            decrypt(LETTERS, key)
         elif z == 3:
-            brute_force(LETTERS)
+            encrypted_message = input("Enter encrypted message:").strip()
+            for i in range(1, 26):
+                brute_force(LETTERS, encrypted_message, i)
+        elif z == 4:
+            a = int(input("Enter num a: "))
+            b = int(input("Enter num b: "))
+            print(egcd(a, b))
+        elif z == 5:
+            a = int(input("Enter num a: "))
+            n = int(input("Enter num n: "))
+            print(inverse_mod(a, n))
         else:
             return main()
 
